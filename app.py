faf541f7fe61bb0b0b17702529d056a3ceaf17a2
Fer Aguirre <kay_op@protonmail.com>
2022-06-26 17:11:09 -0500

    Set title page and favicon

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+
 
 
 def main():
+    # Open image with PIL
+    favicon = Image.open(f'./assets/icon.png')
+    # Set title page and favicon
+    st.set_page_config(
+        page_title='Directorix Disidente', 
+        page_icon = favicon, 
+        layout = 'wide', 
+        initial_sidebar_state = 'expanded',)
     # Set streamlit app title
     st.markdown(
         """
         <h1 style='text-align: center; color: #7b0cd7;'>Directorix Disidente</h1>
+        <div id='header'>
+            <a href='https://www.instagram.com/lacomuna.lenchatrans/'
+            style='text-align:center;display:block'
+            rel='noopener noreferrer'
+            target='_blank'>
+            <b>Comuna Lencha Trans</b>
+        </div>
+
+        <style>
+        #header a {
+
+           color: hotpink;
+            }
+        </style>
         """, unsafe_allow_html=True)
 
     # Read xlsx file as dataframe
@@ -20,52 +45,58 @@ def main():
     categories_cleanned = categories[~pd.isnull(categories)]
     # Insert value to numpy array
     categories_cleanned = np.insert(categories_cleanned, 0, '')
-    # Select a value to filter from dataframe
-    selected_category = st.selectbox('Selecciona una categoría:', sorted(categories_cleanned), index=0)
-
-    results1 = df.loc[df['Habilidades1'] == selected_category]
-    results2 = df.loc[df['Habilidades2'] == selected_category]
-    results3 = df.loc[df['Habilidades3'] == selected_category]
-    results4 = df.loc[df['Habilidades4'] == selected_category]
-    results = pd.concat([results1, results2, results3, results4])
-    # Filter data according to selection
-    if selected_category:
-        for i in results.index:
-            name = df['Nombre'].loc[[i]].item()
-            mail = df['Correo'].loc[[i]].item()
-            contact = df['Contacto'].loc[[i]].item()
-            phone = df['Teléfono'].loc[[i]].item()
-            description = df['Descripción'].loc[[i]].item()
-            description_work = df['Descripción_Trabajo'].loc[[i]].item()
-            contribution = df['Aportación'].loc[[i]].item()
-            with st.expander(name, expanded=False):
-                st.markdown(f'{description}')
-                st.markdown('---')
-                st.markdown(f':envelope_with_arrow: {mail}')
-                st.markdown(f':wave: {contact}')
-                st.markdown(f':telephone_receiver: {phone}')
-                st.markdown(
-                    f"""
-                    <p style='color:#061bed;'>Descripción de mi trabajo:</p>  \n{description_work}
-                    """, unsafe_allow_html=True)
-                st.markdown(
-                    f"""
-                    <p style='color:#061bed;'>¿Cómo deseo aportar a quienes me contacten?</p>  \n{contribution}
-                    """, unsafe_allow_html=True)
+    # Insert containers laid out as columns
+    col1, col2, col3= st.columns([2, 4, 2])
+    with col2:
+        # Select a value to filter from dataframe
+        selected_category = st.selectbox('Selecciona una categoría:', sorted(categories_cleanned), index=0)
+        results1 = df.loc[df['Habilidades1'] == selected_category]
+        results2 = df.loc[df['Habilidades2'] == selected_category]
+        results3 = df.loc[df['Habilidades3'] == selected_category]
+        results4 = df.loc[df['Habilidades4'] == selected_category]
+        results = pd.concat([results1, results2, results3, results4])
+        # Filter data according to selection
+        if selected_category:
+            for i in results.index:
+                name = df['Nombre'].loc[[i]].item()
+                mail = df['Correo'].loc[[i]].item()
+                contact = df['Contacto'].loc[[i]].item()
+                phone = df['Teléfono'].loc[[i]].item()
+                description = df['Descripción'].loc[[i]].item()
+                description_work = df['Descripción_Trabajo'].loc[[i]].item()
+                contribution = df['Aportación'].loc[[i]].item()
+                with st.expander(name, expanded=False):
+                    st.markdown(f'{description}')
+                    st.markdown('---')
+                    st.markdown(f':envelope_with_arrow: {mail}')
+                    st.markdown(f':wave: {contact}')
+                    st.markdown(f':telephone_receiver: {phone}')
+                    st.markdown(
+                        f"""
+                        <p style='color:#061bed;'>Descripción de mi trabajo:</p>  \n{description_work}
+                        """, unsafe_allow_html=True)
+                    st.markdown(
+                        f"""
+                        <p style='color:#061bed;'>¿Cómo deseo aportar a quienes me contacten?</p>  \n{contribution}
+                        """, unsafe_allow_html=True)
     # Add footer link
     st.markdown(
         """
         ---
-        <a href='https://docs.google.com/forms/d/e/1FAIpQLSeI73Pw3YGpsF0JqapU1UoqWlwCarbnIL2YEcONSMIZ14ZvLQ/viewform?pli=1'
-        style='text-align:center;display:block'>
-        <b>:fire: Llena este formulario para formar parte del Directorix Disidente :fire:</b>
+        <div id='footer'>
+            <a href='https://docs.google.com/forms/d/e/1FAIpQLSeI73Pw3YGpsF0JqapU1UoqWlwCarbnIL2YEcONSMIZ14ZvLQ/viewform?pli=1'
+            style='text-align:center;display:block'
+            rel='noopener noreferrer'
+            target='_blank'>
+            <b>¿Quieres formar parte del <i>Directorix Disidente</i>?</b>
+        </div>
 
         <style>
-        a {
+        #footer a {
            text-decoration:None;
+           color: orange;
             }
         </style>
-        </div>
         """, unsafe_allow_html=True)
 
     # Hide streamlit menu and footer message
