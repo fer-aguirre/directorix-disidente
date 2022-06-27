d4d322ef3b1aac4b218b6a10605a784f0228d83a
Fer Aguirre <kay_op@protonmail.com>
2022-06-27 16:24:40 -0500

    Update data, project description and url

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from PIL import Image
 
 
 def main():
-
     # Open image with PIL
     favicon = Image.open(f'./assets/icon.png')
     # Set title page and favicon
@@ -21,23 +20,20 @@ def main():
         """
         <h1 style='text-align: center; color: #7b0cd7;'>Directorix Disidente</h1>
         """, unsafe_allow_html=True)
-
     # Insert containers laid out as columns
     col1, col2, col3= st.columns([3, 6, 3])
     # Set streamlit app description
     with col2:
-
         st.markdown(
         """
         <p style='text-align: justify;'>
-            Te damos la bienvenida a este directorix dónde nos dimos a la tarea de  
-            reunir comunidad disidente de la que puedas apoyarte para concluir proyectos,  
-            materializar ideas y cualquier otro tipo de apoyo o acompañamiento. Esperamos
-            que en esta red encuentres a quien buscas y juntxs construyamos cosas increíbles.
-            <span id='description'>
-                <a href='https://www.instagram.com/lacomuna.lenchatrans/' rel='noopener noreferrer' target='_blank'>
-                <b>La Comuna Lencha Trans</b>
-            </span>
+            En <span id='description'>
+            <a href='https://www.instagram.com/lacomuna.lenchatrans/' rel='noopener noreferrer' target='_blank'>
+            La Comuna Lencha Trans</a></span>
+             te damos la bienvenida a este directorix dónde nos dimos a la tarea de  
+            reunir a comunidad disidente de la que puedas apoyarte para concluir proyectos,  
+            materializar ideas y cualquier otro tipo de apoyo o acompañamiento que desees. 
+            Esperamos que en esta red encuentres a quien buscas y juntxs construyamos cosas increíbles.
         </p>
         <style>
         #description a {
@@ -46,13 +42,12 @@ def main():
             }
         </style>
         """, unsafe_allow_html=True)
-
     # Read xlsx file as dataframe
-    df = pd.read_excel('data/directorix-processed.xlsx', converters={'Teléfono': str})
+    df = pd.read_excel('./data/directorix-processed.xlsx', converters={'telefono': str})
     # Fill nan values on columns with a values
-    df.fillna({'Teléfono':'----', 'Descripción_Trabajo':'', 'Aportación':''}, inplace=True)
+    df.fillna({'telefono':'----', 'descripcion_trabajo':'', 'aportacion':''}, inplace=True)
     # Get unique values from several columns
-    categories = pd.unique(df[['Habilidades1', 'Habilidades2', 'Habilidades3', 'Habilidades4']].values.ravel())
+    categories = pd.unique(df[['habilidades1', 'habilidades2', 'habilidades3', 'habilidades4']].values.ravel())
     # Remove nan values from array
     categories_cleanned = categories[~pd.isnull(categories)]
     # Insert value to numpy array
@@ -62,21 +57,21 @@ def main():
     with col2:
         # Select a value to filter from dataframe
         selected_category = st.selectbox('Selecciona una categoría:', sorted(categories_cleanned), index=0)
-        results1 = df.loc[df['Habilidades1'] == selected_category]
-        results2 = df.loc[df['Habilidades2'] == selected_category]
-        results3 = df.loc[df['Habilidades3'] == selected_category]
-        results4 = df.loc[df['Habilidades4'] == selected_category]
+        results1 = df.loc[df['habilidades1'] == selected_category]
+        results2 = df.loc[df['habilidades2'] == selected_category]
+        results3 = df.loc[df['habilidades3'] == selected_category]
+        results4 = df.loc[df['habilidades4'] == selected_category]
         results = pd.concat([results1, results2, results3, results4])
         # Filter data according to selection
         if selected_category:
             for i in results.index:
-                name = df['Nombre'].loc[[i]].item()
-                mail = df['Correo'].loc[[i]].item()
-                contact = df['Contacto'].loc[[i]].item()
-                phone = df['Teléfono'].loc[[i]].item()
-                description = df['Descripción'].loc[[i]].item()
-                description_work = df['Descripción_Trabajo'].loc[[i]].item()
-                contribution = df['Aportación'].loc[[i]].item()
+                name = df['nombre'].loc[[i]].item()
+                mail = df['correo'].loc[[i]].item()
+                contact = df['contacto'].loc[[i]].item()
+                phone = df['telefono'].loc[[i]].item()
+                description = df['descripcion'].loc[[i]].item()
+                description_work = df['descripcion_trabajo'].loc[[i]].item()
+                contribution = df['aportacion'].loc[[i]].item()
                 with st.expander(name, expanded=False):
                     st.markdown(f'{description}')
                     st.markdown('---')
@@ -91,12 +86,13 @@ def main():
                         f"""
                         <p style='color:#061bed;'>¿Cómo deseo aportar a quienes me contacten?</p>  \n{contribution}
                         """, unsafe_allow_html=True)
+
     # Add footer link
     st.markdown(
         """
         ---
         <div id='footer'>
-            <a href='https://docs.google.com/forms/d/e/1FAIpQLSeI73Pw3YGpsF0JqapU1UoqWlwCarbnIL2YEcONSMIZ14ZvLQ/viewform?pli=1'
+            <a href='https://ee.humanitarianresponse.info/x/s5znI8NZ'
             style='text-align:center;display:block'
             rel='noopener noreferrer'
             target='_blank'>
